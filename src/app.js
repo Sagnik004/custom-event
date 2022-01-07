5a76926d045398fc1f10d1d001f416723395d2a9
Sagnik004 <sagnikchakraborty910@yahoo.com>
2022-01-08 02:08:04 +0530

    init commit

---
+++ src/app.js
@@ -0,0 +1,50 @@
+// DOM Elements
+const todosContainer = document.getElementById("todos-container");
+
+// Functions
+const fetchAndDispatch = async function (url, domElementToNotify, eventTitle) {
+  const results = await fetch(url);
+  const data = await results.json();
+
+  // Create custom event
+  const customEvent = new CustomEvent(eventTitle, {
+    detail: {
+      data,
+    },
+  });
+
+  // Dispatch event
+  domElementToNotify.dispatchEvent(customEvent);
+};
+
+const createTodo = function ({ id, title, completed }) {
+  const todoEl = document.createElement("div");
+  todoEl.classList.add("todo");
+
+  const todoTitle = document.createElement("h3");
+  todoTitle.classList.add("todo-title");
+  todoTitle.textContent = title;
+
+  const todoStatus = document.createElement("div");
+  todoStatus.classList.add("todo-status");
+  todoStatus.textContent = completed ? "Complete" : "Incomplete";
+
+  todoEl.append(todoTitle, todoStatus);
+  return todoEl;
+};
+
+const handleTodosLoaded = function (e) {
+  const todos = e.detail.data;
+  todos.forEach((todo) => todosContainer.append(createTodo(todo)));
+};
+
+// Event Listeners
+document.addEventListener("todos-loaded", handleTodosLoaded);
+
+window.addEventListener("DOMContentLoaded", () => {
+  fetchAndDispatch(
+    "https://jsonplaceholder.typicode.com/todos",
+    document,
+    "todos-loaded"
+  );
+});
